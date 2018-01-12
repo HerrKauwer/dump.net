b6d0420e7a1ca5ec3928b7c3078562eb95e6cfa0
HerrKauwer <HerrKauwer@users.noreply.github.com>
2018-01-12 01:11:52 +0100

    Added resumable.js. Added ASP.NET WebApi. Updated Bootstrap

--- Models/FileInfo.cs
+++ Models/FileInfo.cs
@@ -9,22 +9,8 @@ namespace Dump.Models
     {
         public string FileName { get; set; }
 
-        internal long Size { get; set; }
+        public long Size { get; set; }
 
         public string LastModified { get; set; }
-
-        public string ReadableSize
-        {
-            get
-            {
-                string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
-                if (Size == 0)
-                    return "0" + suf[0];
-                long bytes = Math.Abs(Size);
-                int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
-                double num = Math.Round(bytes / Math.Pow(1024, place), 1);
-                return (Math.Sign(Size) * num).ToString() + " " + suf[place];
-            }
-        }
     }
 }
--- Scripts/dump.js
+++ Scripts/dump.js
@@ -20,11 +20,38 @@ function refreshFiles() {
             td.append($("<a>").text(fileMeta.FileName).attr("href", "/Home/Download?filename=" + fileMeta.FileName))
             tr.append($(td));
             tr.append($("<td>").text(fileMeta.LastModified));
-            tr.append($("<td>").text(fileMeta.ReadableSize));
+            tr.append($("<td>").text(readableSize(fileMeta.Size)));
 
             tbody.append(tr);
         });
     });
+}
+
+function readableSize(size) {
+    suf = ["B", "KB", "MB", "GB", "TB", "PB", "EB"]; //Longs run out around EB
+    if (size == 0)
+        return "0" + suf[0];
+    var bytes = Math.abs(size);
+    var place = Math.floor(Math.log(bytes) / Math.log(1024));
+    var num = Math.round(bytes / Math.pow(1024, place), 1);
+    return (Math.sign(size) * num).toString() + " " + suf[place];
+}
+
+function addFileToTable(file) {
+    var tbody = $("#progress tbody");
+    var uploads = $("tr", tbody);
+
+    var tr = $("<tr>").data("filename", file.fileName);
+    tr.append($("<td>").text(file.fileName));
+    tr.append($("<td>").text(readableSize(file.size)));
+    var td = $("<td>");
+    var progress = $("<div>").addClass("progress");
+    td.append(progress);
+    var progressBar = $("<div>").addClass("progress-bar").addClass("progress-bar-striped").addClass("active").css("width", "0%");
+    progress.append(progressBar);
+    tr.append(td);
+
+    tbody.append(tr);
 }
 
 $().ready(function () {
@@ -47,5 +74,42 @@ $().ready(function () {
         event.stopPropagation();
 
         refreshFiles();
+    });
+
+    var r = new Resumable({
+        target: '/api/File/Upload'
+    });
+
+    if (!r.support) {
+        $("#legacy").show();
+        $("#new").hide();
+    }
+
+    r.assignBrowse(document.getElementById('browse-button'));
+
+    r.on('fileSuccess', function (file) {
+        refreshFiles();
+    });
+    r.on('fileProgress', function (file) {
+        var tbody = $("#progress tbody");
+        var uploads = $("tr", tbody);
+
+        var percentage = Math.floor(file.progress() * 100).toString();
+
+        $.each(uploads, function (i, tr) {
+            if ($(tr).data("filename") === file.fileName) {
+                $(".progress-bar", tr).css("width", percentage + "%");
+                $(".progress-bar", tr).text(percentage + "%");
+
+                if (percentage === "100") {
+                    $(".progress-bar", tr).removeClass("progress-bar-striped").removeClass("progress-bar-animated");
+                }
+            }
+        });
+    });
+    r.on('fileAdded', function (file, event) {
+        r.upload();
+
+        addFileToTable(file);
     });
 });
